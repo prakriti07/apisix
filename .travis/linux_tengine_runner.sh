f8fc0ffb90fdd7b08c04774819f5b4ec825dc9aa
prakriti07 <57622944+prakriti07@users.noreply.github.com>
2020-08-11 14:59:08 +0530

    Update linux_tengine_runner.sh

--- .travis/linux_tengine_runner.sh
+++ .travis/linux_tengine_runner.sh
@@ -63,6 +63,7 @@ tengine_install() {
         return
     fi
 
+    sudo apt-get -y install libpcre3-dev libssl-dev perl make build-essential curl zlib1g zlib1g-dev unzip
     export OPENRESTY_VERSION=1.15.8.3
     wget https://openresty.org/download/openresty-$OPENRESTY_VERSION.tar.gz
     tar zxf openresty-$OPENRESTY_VERSION.tar.gz
