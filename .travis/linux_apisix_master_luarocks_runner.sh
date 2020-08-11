fea155f522582d188072cbbd0fd91796aa9e8433
prakriti07 <57622944+prakriti07@users.noreply.github.com>
2020-08-11 16:26:27 +0530

    Update linux_apisix_master_luarocks_runner.sh

--- .travis/linux_apisix_master_luarocks_runner.sh
+++ .travis/linux_apisix_master_luarocks_runner.sh
@@ -24,7 +24,7 @@ export_or_prefix() {
 }
 
 do_install() {
-    export OPENRESTY_VERSION=1.15.8.3
+    export OPENRESTY_VERSION=1.17.8.1
     export OPENRESTY_PREFIX="/usr/local/openresty-debug"
     sudo apt-get -y install libpcre3-dev libssl-dev perl make build-essential curl zlib1g zlib1g-dev unzip
     wget https://openresty.org/download/openresty-$OPENRESTY_VERSION.tar.gz
