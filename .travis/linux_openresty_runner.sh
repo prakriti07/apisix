9bc5112f20f9e7f8173368c14f7a22c4ca173081
prakriti07 <57622944+prakriti07@users.noreply.github.com>
2020-08-12 01:09:38 +0530

    Update linux_openresty_runner.sh

--- .travis/linux_openresty_runner.sh
+++ .travis/linux_openresty_runner.sh
@@ -54,11 +54,12 @@ before_install() {
 
 do_install() {
     export_or_prefix
-    if [ "$(uname -m)" == "aarch64" ]; then
+    if [ "$(uname -m)" == "aarch64" ] 
+    then
         arch="arm64"
         echo $(arch)
     else
-        arch="amd64"
+        export arch="amd64"
         echo $(arch)
     fi
     export OPENRESTY_VERSION=1.17.8.1
